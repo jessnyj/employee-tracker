0e693b383526c100a9e956c9a1cb850dab713aa7
jessny <jessnyjoseph@gmail.com>
2021-01-27 20:40:04 -0800

    created starter prompt

--- employee.js
+++ employee.js
@@ -1,23 +1,35 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+const { start } = require("repl");
 
 // create the connection information for the sql database
 var connection = mysql.createConnection({
-  host: "localhost",
+    host: "localhost",
 
-  // Your port; if not 3306
-  port: 3306,
+    // Your port; if not 3306
+    port: 3306,
 
-  // Your username
-  user: "root",
+    // Your username
+    user: "root",
 
-  // Your password
-  password: "password",
-  database: "employeeDB"
+    // Your password
+    password: "password",
+    database: "employeeDB"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    afterConnection();
-  });
+    start();
+});
+
+function start() {
+    inquirer
+        .prompt({
+            name: "starterQs",
+            type: "rawlist",
+            message: "What would you like to do?",
+            choices: ["View all Employees", "View all Roles", "View Departments", "Add Employee", "Add Department", "Add Role", "Update Employee Role"]
+
+        })
+}
