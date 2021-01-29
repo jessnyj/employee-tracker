f2bebbdf1d2a9e51d4a78705e970bb1f9500f76e
jessny <jessnyjoseph@gmail.com>
2021-01-28 20:18:57 -0800

    added role table, started intitial functionality of add employee

--- employee.js
+++ employee.js
@@ -42,23 +42,23 @@ function employeeTable() {
     });
 }
 
-// function roleTable() {
-//     connection.query("SELECT * FROM employee", function (err, res) {
+function roleTable() {
+    connection.query("SELECT * FROM role", function (err, res) {
 
-//         var table = new Table({
-//             head: ["ID", "First Name", "Last Name", "Role ID", "Manager ID"],
-//             colWidths: [10, 10, 10, 10, 10]
-//         });
+        var table = new Table({
+            head: ["ID", "Title", "Salary", "Department Id"],
+            colWidths: [10, 10, 10, 10, 10]
+        });
 
-//         for (var i = 0; i < res.length; i++) {
-//             table.push(
-//                 [res[i].id, res[i].first_name, res[i].last_name, res[i].role_id, res[i].manager_id],
-//             );
-//         }
-//         console.log(table.toString());
-//         restart()
-//     });
-// }
+        for (var i = 0; i < res.length; i++) {
+            table.push(
+                [res[i].id, res[i].title, res[i].salary, res[i].department_id],
+            );
+        }
+        console.log(table.toString());
+        restart()
+    });
+}
 
 // Initial Prompt
 function manageEmployees() {
@@ -68,44 +68,56 @@ function manageEmployees() {
             type: "rawlist",
             message: "What would you like to do?",
             choices: [
-                "View all Employees", 
-                "View all Roles", 
-                "View Departments", 
-                "Add Employee", 
-                "Add Department", 
-                "Add Role", 
+                "View all Employees",
+                "View all Roles",
+                "View Departments",
+                "Add Employee",
+                "Add Department",
+                "Add Role",
                 "Update Employee Role"
             ]
         })
-        .then(function(answer) {
+        .then(function (answer) {
             switch (answer.starterQs) {
-            case "View all Employees":
-                employeeTable();
-                break;
-            case "View all Employees by Department":
-                roleView();
-                break;
-            case "View all Employees by Manager":
-                departmentView();
-                break;
-            case "Add Employee":
-                employeeAdd();
-                break;
-            case "Add Department":
-                departmentAdd();
-                break;
-            case "Add Role":
-                roleAdd;
-                break;
-            case "Update Employee Role":
-                updateRole();
-                break;
+                case "View all Employees":
+                    employeeTable();
+                    break;
+                case "View all Roles":
+                    roleTable();
+                    break;
+                case "View Departments":
+                    departmentView();
+                    break;
+                case "Add Employee":
+                    employeeAdd();
+                    break;
+                case "Add Department":
+                    departmentAdd();
+                    break;
+                case "Add Role":
+                    roleAdd;
+                    break;
+                case "Update Employee Role":
+                    updateRole();
+                    break;
             }
         })
-    }
+}
+
 
 // Add Employee
-    function add() {
+function employeeAdd() {
+    connection.query("SELECT title, id FROM role", function (err, res) {
+        var titleRole = [];
+        var roleId = [];
+        for (var i = 0; i < res.length; i++) {
+            var temp = {title: res[i].title, id: res[i].id}
+            titleRole.push(
+                res[i].title
+            );
+            roleId.push(temp);
+        }
+        console.log(roleId);
         inquirer.prompt([{
             name: "firstName",
             message: "\nEnter the Employees first name\n"
@@ -113,37 +125,43 @@ function manageEmployees() {
             name: "lastName",
             message: "Enter the Employees last name\n"
         }, {
+            type: 'list',
             name: "role",
-            message: "What is the employee's role?\n"
+            message: "What is the employee's role?\n",
+            choices: titleRole,
         }, {
-            name: "manager",
-            message: "Who is the employee's manager?\n"
-    
+            // name: "manager",
+            // message: "Who is the employee's manager?\n"
+
         }]).then(function (answers) {
+            console.log(answers.role);
             connection.query("INSERT INTO employee SET ?", {
                 first_name: answers.firstName,
                 last_name: answers.lastName,
                 role_id: answers.role,
-                manager_id: answers.manager
+                // manager_id: answers.manager
             })
             employeeTable();
         })
+
     }
-        
-    
+
+    )
+}
 // Start Over
-    function restart() {
-        inquirer.prompt([{
-            type: "list",
-            name: "continue",
-            choices: ["Yes", "No"],
-            message: "Would you like to do more?\n"
-        }]).then(function (answers) {
-            if (answers.continue === "Yes") {
-                manageEmployees();
-            } else {
-                exit()
-            }
-    
-        });
-    }
+function restart() {
+    inquirer.prompt([{
+        type: "list",
+        name: "continue",
+        choices: ["Yes", "No"],
+        message: "Would you like to do more?\n"
+    }]).then(function (answers) {
+        if (answers.continue === "Yes") {
+            manageEmployees();
+        } else {
+            exit()
+        }
+
+    });
+}
+manageEmployees();
--- employee.sql
+++ employee.sql
@@ -4,13 +4,13 @@ CREATE database employeeDB;
 USE employeeDB;
 
 CREATE TABLE department (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30),
     PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30),
     salary DECIMAL(10,4) NULL,
     department_id INT NOT NULL,
@@ -19,7 +19,7 @@ CREATE TABLE role (
 
 
 CREATE TABLE employee (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT NOT NULL,
@@ -33,5 +33,7 @@ VALUES ("Sales"), ("Finance"), ("Legal"), ("Engineering");
 INSERT INTO role (title, salary, department_id)
 VALUES ("Sales Lead", 100000, 100), ("Account Manager", 160000, 45), ("Accountant", 125000, 120), ("Account Manager", 160000, 16);
 
+
+-- manager and dept id diff
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("John", "Doe", 23, null), ("Danielle", "Matthews", 70, 45), ("Nick", "Jones", 77, null), ("Natalie", "Martinez", 10, 16);
